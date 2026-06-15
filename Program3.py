431d6dae043996eeacad21ecc2188d97bb346bf7
vishavbharti <vishavbharti05@gmail.com>
2026-06-15 16:49:52 +0530

    Delete Program3.py

--- Program3.py
+++
@@ -1,6 +0,0 @@
-#Find unique characters in a string using sets
-text = input("Enter a string : ")
-unique_char = set(text)
-print("Unique characters are : ")
-for char in unique_char :
-    print(char , end =" ")
